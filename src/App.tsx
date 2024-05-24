cfd3023e099abcca1f6eb7f60fa4a7c6f442dba7
sahinpathan <pathansahin1995@gmail.com>
2024-05-24 18:18:50 +0530

    component name changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import SimpleDialogDemo from "./components/SimpleDialog"
-import { PdfPage } from "./components/ViewPdf/pdfPage"
-import { SignatureProvider } from "./context/SignatureProvider"
+import { SignPdf } from "./SignPdf"
 
 
 function App() {
 
   return (
     <>
-      <SignatureProvider>
-        <div className='container'>
-          <SimpleDialogDemo title={"Upload Signature"} isUpload={true} />
-          <SimpleDialogDemo title={"Draw Signature"} />
-        </div>
-        <PdfPage />
-      </SignatureProvider>
+      <SignPdf/>
     </>
   )
 }
--- src/components/imageCropper/useDebounceEffect.tsx
+++ src/components/imageCropper/useDebounceEffect.tsx
@@ -7,7 +7,7 @@ export function useDebounceEffect(
 ) {
   useEffect(() => {
     const t = setTimeout(() => {
-      fn.apply(undefined, deps)
+      fn()
     }, waitTime)
 
     return () => {
